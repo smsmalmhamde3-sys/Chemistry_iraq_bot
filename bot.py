a930ea7859174a9b2aa7d603f71eb6cb76f6ca26
smsmalmhamde3-sys <smsmalmhamde3@gmail.com>
2026-06-29 18:54:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("8976857022:AAFnN6TTcmN38MzThnXuqUlFqf4tkYsUR3k")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("البوت شغال 👍")
